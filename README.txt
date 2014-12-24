bc135a58be5466befe248640c24e6a9a97ad7621
Chris AtLee <catlee@mozilla.com>
2014-12-24 18:37:46 -0500

    Initial import

---
+++ README.txt
@@ -0,0 +1,1 @@
+Utility for managing mar files. MAR file format is documented at https://wiki.mozilla.org/Software_Update:MAR
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="mar",
+    version="1.0",
+    author="Chris AtLee",
+    author_email="catlee@mozilla.com",
+    packages=find_packages(),
+    url="https://github.com/mozilla/build-mar",
+    license="MPL 2.0",
+    description="MAR (Mozilla ARchive) Python implementation",
+    long_description=open('README.txt').read(),
+    scripts=["mar/mar.py"],
+)
