f87066b6acb3d8cc57950195d49c76028535e938
Rail Aliiev <rail@mozilla.com>
2014-12-26 11:23:46 -0500

    v1.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mar",
-    version="1.0",
+    version="1.0.1",
     author="Chris AtLee",
     author_email="catlee@mozilla.com",
     packages=find_packages(),
