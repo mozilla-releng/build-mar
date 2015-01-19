3d791522db837e55d0de89f9a28679e6a2538936
Chris AtLee <catlee@mozilla.com>
2015-01-19 12:13:15 -0500

    Add some tests for BZ2MarFile

--- tests/test_mar.py
+++ tests/test_mar.py
@@ -4,9 +4,10 @@ import os
 import tempfile
 import hashlib
 
-from mar.mar import MarFile
+from mar.mar import MarFile, BZ2MarFile
 
 TEST_MAR = os.path.join(os.path.dirname(__file__), 'test.mar')
+
 
 def sha1sum(b):
     """Returns the sha1sum of a byte string"""
@@ -17,6 +18,10 @@ def sha1sum(b):
 
 def test_list():
     m = MarFile(TEST_MAR)
+    assert repr(m.members[0]) == "<update.manifest 664 141 bytes starting at 392>", m.members[0]
+    assert repr(m.members[1]) == "<defaults/pref/channel-prefs.js 664 76 bytes starting at 533>", m.members[1]
+
+    m = BZ2MarFile(TEST_MAR)
     assert repr(m.members[0]) == "<update.manifest 664 141 bytes starting at 392>", m.members[0]
     assert repr(m.members[1]) == "<defaults/pref/channel-prefs.js 664 76 bytes starting at 533>", m.members[1]
 
@@ -41,6 +46,30 @@ class TestMar(TestCase):
         data = open(fn, 'rb').read()
         h = sha1sum(data)
         self.assertEquals("6a7890e740f1e18a425b51fefbde2f6b86f91a12", h)
+
+
+class TestBZ2Mar(TestCase):
+    def setUp(self):
+        self.tmpdir = tempfile.mkdtemp()
+        self.marfile = BZ2MarFile(TEST_MAR)
+
+    def tearDown(self):
+        shutil.rmtree(self.tmpdir)
+
+    def test_extract_bz2(self):
+        m = self.marfile.members[0]
+        self.marfile.extract(m, self.tmpdir)
+        fn = os.path.join(self.tmpdir, m.name)
+
+        # The size in the manifest is of the compressed data, so we need to
+        # check that we've extracted the correct number of uncompressed bytes
+        # here
+        self.assertEquals(os.path.getsize(fn), 308)
+
+        # Check that the contents match
+        data = open(fn, 'rb').read()
+        h = sha1sum(data)
+        self.assertEquals("5177f5938923e94820d8565a1a0f25d19b4821d1", h)
 
 
 class TestExceptions(TestCase):
