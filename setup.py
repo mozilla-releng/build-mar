b0cfff355d96ef9c0e04dfe4bf88aa6a33c6b39f
Chris AtLee <catlee@mozilla.com>
2015-01-20 14:52:37 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="mar",
-    version="1.1",
+    version="1.2",
     author="Chris AtLee",
     author_email="catlee@mozilla.com",
     packages=find_packages(),
