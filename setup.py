52b197823450c2de1aa4605f5869c42b2f4988c4
Chris AtLee <catlee@mozilla.com>
2016-11-01 16:58:42 -0400

    fixing tox

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ from setuptools import setup, find_packages
 
 setup(
     name="mar",
-    version="1.3pre",
+    version="1.4pre",
     author="Chris AtLee",
     author_email="catlee@mozilla.com",
     packages=find_packages(),
     url="https://github.com/mozilla/build-mar",
     license="MPL 2.0",
     description="MAR (Mozilla ARchive) Python implementation",
+    install_requires=open('requirements.txt').readlines(),
     long_description=open('README.md').read(),
     scripts=["scripts/mar.py"],
 )
