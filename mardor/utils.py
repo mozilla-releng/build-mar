5d64ef597dbbecc4077606c832c8814dd0190d8e
Chris AtLee <catlee@mozilla.com>
2016-11-01 17:33:04 -0400

    fix binary mode

--- mardor/utils.py
+++ mardor/utils.py
@@ -75,4 +75,4 @@ def openfile(filename_or_fileobj, mode='r'):
     if hasattr(filename_or_fileobj, 'read'):
         return filename_or_fileobj
     else:
-        return open(filename_or_fileobj, mode)
+        return open(filename_or_fileobj, mode + 'b')
