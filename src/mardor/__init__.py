7081aa058ee0a5bb83559394020d0a287d11cc00
Chris AtLee <catlee@mozilla.com>
2017-09-06 15:55:45 -0400

    Bump version

--- src/mardor/__init__.py
+++ src/mardor/__init__.py
@@ -8,5 +8,5 @@ Signing and verification of MAR files are supported.
 
 The primary modules of interest are `mardor.reader` and `mardor.writer`
 """
-version = (2, 2, 1)
-version_str = "2.2.1"
+version = (2, 2, 2)
+version_str = "2.2.2"
