0648fcb49b18fcb0db9fcf8a54d6ce6aa71218dd
Chris AtLee <catlee@mozilla.com>
2017-09-07 21:51:33 -0400

    Update changelog; add tests that we've updated the changelog

--- tests/test_version.py
+++ tests/test_version.py
@@ -1,3 +1,6 @@
+import os
+import re
+
 from pkg_resources import get_distribution
 
 import mardor
@@ -7,3 +10,12 @@ def test_version():
     dist = get_distribution('mar')
     assert mardor.version_str == dist.version
     assert ".".join(str(_) for _ in mardor.version) == dist.version
+
+
+def test_version_in_changelog():
+    dist = get_distribution('mar')
+    here = os.path.abspath(os.path.dirname(__file__))
+    changelog_path = os.path.join(here, '..', 'CHANGELOG.rst')
+    changelog = open(changelog_path).read()
+    assert re.search('^{}'.format(re.escape(mardor.version_str)), changelog,
+                     re.M)
