615691dc76195bab1d7367498fd1f93f7cf072a1
Chris AtLee <chris@atlee.ca>
2019-02-01 15:20:39 -0500

    Bump dependencies
    pytest 4 broke the old version of pytest-random-order we were using,
    since the newer versions of pytest-random-order don't work with py2.
    
    I solved this by creating separate requirements files for py2 and py3.

--- update-requirements.sh
+++ update-requirements.sh
@@ -4,3 +4,6 @@ rm *requirements.txt || true
 
 pip-compile --generate-hashes --output-file requirements.txt requirements.in
 pip-compile --generate-hashes --output-file test-requirements.txt test-requirements.in
+
+pip-compile2 --generate-hashes --output-file requirements-py2.txt requirements.in
+pip-compile2 --generate-hashes --output-file test-requirements-py2.txt test-requirements.in
